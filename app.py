9c8637fc240a6d0c5c16c6c59185a01e6a7988e9
AnKnicely <151943483+AnKnicely@users.noreply.github.com>
2024-11-14 16:47:12 -0500

    Update app.py
    Adding in the most popular creator

--- app.py
+++ app.py
@@ -9,16 +9,20 @@ most_popular = df[df['playCount'] == df['playCount'].max()]
 id = most_popular['id']
 #use this id to find the file on desktop
 
-st.title('The most Popular video is:')
+st.title('Welcome to the Tik Tok Dashboard')
+st.write('below.....')
+
+
+st.subheader('Most Popular Video')
 video_file = open("6894081763379924229.mp4", 'rb')
 video_bytes = video_file.read()
 st.video(video_bytes)
 
 creator =  most_popular['authorMeta_name']
 desc = most_popular['text']
-st.write(creator)
-st.write(desc)
-st.write(most_popular['playCount'])
+st.write(f"Creator Name: {most_popular['authorMeta_name'].values[0]}")
+st.write(f"Description: {most_popular['text'].values[0]} ")
+st.write(f"Play Count: {most_popular['playCount'].values[0]} ")
 
 #TO DO: Find the most popular video and find the most popular song
 #most popular tiktoker
@@ -27,17 +31,13 @@ col1, col2 = st.columns(2)
 most_popular_song = df[df['playCount'] == df.groupby('musicMeta_musicName')['playCount'].transform('max')]
 col1.subheader('The Most Popular Song')
 col1.write(f"Audio Title: {most_popular_song['musicMeta_musicName'].values[0]}")
+col1.write(f"Audio Creator: {most_popular_song['musicMeta_musicAuthor'].values[0]}")
 col1.write(f"Audio Play Count: {most_popular_song['playCount'].values[0]}")
 
 
-print(most_popular_song)
-most_popular_tiktoker = df[df['playCount'] == df.groupby('authorMeta_name')['playCount'].transform('max')]
 
-most_popular_song = df[df['playCount'] == df.groupby('musicMeta_musicName')['playCount'].transform('max')]
-#col1.subheader('The Most Popular Song')
-#col1.write(f"Audio Title: {most_popular_song['musicMeta_musicName'].values[0]}")
-#col1.write(f"Audio Play Count: {most_popular_song['playCount'].values[0]}")
-
-print('help')
-print(most_popular_song)
 most_popular_tiktoker = df[df['playCount'] == df.groupby('authorMeta_name')['playCount'].transform('max')]
+col2.subheader('Most Popular Creator')
+col2.write(f"Creator Name: {most_popular_tiktoker['authorMeta_name'].values[0]}")
+col2.write(f"Views: {most_popular_tiktoker['playCount'].values[0]}")
+col2.write(f"Verified? {most_popular_tiktoker['authorMeta_verified'].values[0]}")
