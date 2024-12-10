42338e94628d46303edfbab71cc6e4433e0dbb14
AnKnicely <151943483+AnKnicely@users.noreply.github.com>
2024-12-10 12:13:07 -0500

    Update app.py

--- app.py
+++ app.py
@@ -72,7 +72,7 @@ elif page == "Sound Analytics":
         fig = px.histogram(filtered_df, 
                            x='musicMeta_musicName', 
                            y= 'playCount',
-                          color = 'commentCount')
+                          color = 'playCount')
         st.plotly_chart(fig, use_container_width= True)
 
         left_col, right_col = st.columns(2)
